b9b774fb73a43c291d76bdadfc76639a07c656e3
rayhanssm <rayhansmantiri@gmail.com>
2022-12-26 20:28:27 +0700

    Update CV's link

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
           <h1 className='text-3xl sm:text-5xl font-black mb-8 text-[#7EDDFC]'>About Me</h1>
           <p className='mb-8 sm:mb-4'>I am a computer science student, currently studying at Bina Nusantara University. I've been designing since vocational high school, and my parents used to ask me to design for them.</p>
           <p className='mb-8 sm:mb-4'>I'm interested in Front-End development and UI/UX because it's in line with my major and I want to learn more about it, for making a better interface that can be used for anyone.</p>
-          <button onClick={() => openLink('https://drive.google.com/file/d/1m1bOX8NqdORfli6rhTrsBoNOhxXaiq3K/view?usp=share_link')} className='border-2 rounded-md my-2 px-5 py-2  flex items-center hover:-translate-y-1 hover:scale-110 hover:bg-[#FF304F] hover:border-[#002651] hover:shadow-md duration-200'>View CV</button>
+          <button onClick={() => openLink('https://drive.google.com/file/d/1g_e7TR27IlIAIcM-VSMlyo9KRupU7qk-/view')} className='border-2 rounded-md my-2 px-5 py-2  flex items-center hover:-translate-y-1 hover:scale-110 hover:bg-[#FF304F] hover:border-[#002651] hover:shadow-md duration-200'>View CV</button>
         </div>
         <img src={AboutPic} alt="About Image" style={{width: '432px'}}/>
       </div>
