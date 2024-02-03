13f4d9c010b05d9c806a5a5ebccf382d41f87fc5
Ray Hans Sebastian Mantiri <rayhansmantiri@gmail.com>
2024-02-03 23:31:54 +0700

    refactor: update cv link

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,24 +1,46 @@
-import React from 'react'
-import AboutPic from '../assets/about.png'
+import React from "react";
+import AboutPic from "../assets/about.png";
 
 const About = () => {
-  const openLink= url => {
-    window.open(url, '_blank', 'noopener,noreferrer')
-  }
+  const openLink = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
 
   return (
-    <div name='about' className='w-full h-auto sm:px-6 bg-[#002651] text-[#FEFEFE] bg-gradient-to-b from-[#FEFEFE10]'>
-      <div className='md:px-24 px-6 md:flex justify-around items-center h-screen'>
-        <div className='pt-28 sm:w-[50%]'>
-          <h1 className='text-3xl sm:text-5xl font-black mb-8 text-[#7EDDFC]'>About Me</h1>
-          <p className='mb-8 sm:mb-4'>I am a computer science student, currently studying at Bina Nusantara University. I've been designing since vocational high school, and my parents used to ask me to design for them.</p>
-          <p className='mb-8 sm:mb-4'>I'm interested in Front-End development and UI/UX because it's in line with my major and I want to learn more about it, for making a better interface that can be used for anyone.</p>
-          <button onClick={() => openLink('https://drive.google.com/file/d/1g_e7TR27IlIAIcM-VSMlyo9KRupU7qk-/view')} className='border-2 rounded-md my-2 px-5 py-2  flex items-center hover:-translate-y-1 hover:scale-110 hover:bg-[#FF304F] hover:border-[#002651] hover:shadow-md duration-200'>View CV</button>
+    <div
+      name="about"
+      className="w-full h-auto sm:px-6 bg-[#002651] text-[#FEFEFE] bg-gradient-to-b from-[#FEFEFE10]"
+    >
+      <div className="md:px-24 px-6 md:flex justify-around items-center h-screen">
+        <div className="pt-28 sm:w-[50%]">
+          <h1 className="text-3xl sm:text-5xl font-black mb-8 text-[#7EDDFC]">
+            About Me
+          </h1>
+          <p className="mb-8 sm:mb-4">
+            I am a computer science student, currently studying at Bina
+            Nusantara University. I've been designing since vocational high
+            school, and my parents used to ask me to design for them.
+          </p>
+          <p className="mb-8 sm:mb-4">
+            I'm interested in Front-End development and UI/UX because it's in
+            line with my major and I want to learn more about it, for making a
+            better interface that can be used for anyone.
+          </p>
+          <button
+            onClick={() =>
+              openLink(
+                "https://drive.google.com/file/d/1okXW3SV86UOlJeVQeZ4ZC20K-KexqB5B/view?usp=sharing"
+              )
+            }
+            className="border-2 rounded-md my-2 px-5 py-2  flex items-center hover:-translate-y-1 hover:scale-110 hover:bg-[#FF304F] hover:border-[#002651] hover:shadow-md duration-200"
+          >
+            View CV
+          </button>
         </div>
-        <img src={AboutPic} alt="About Image" style={{width: '432px'}}/>
+        <img src={AboutPic} alt="About Image" style={{ width: "432px" }} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
